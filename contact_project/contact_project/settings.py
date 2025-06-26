5142ba1f06b1a72a6045ffd28641e9e1c6a7058f
shreyash8055 <149226098+shreyash8055@users.noreply.github.com>
2025-06-26 16:21:40 +0530

    Update settings.py

--- contact_project/contact_project/settings.py
+++ contact_project/contact_project/settings.py
@@ -132,9 +132,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_HOST_USER =''
-EMAIL_HOST_USER = 'shreyash.sunware02@gmail.com'
-EMAIL_HOST_PASSWORD = 'gqoyczoevitzfylw'
-# ADMIN_EMAIL = 'shreyash.sunware02@gmail.com'
-# ADMIN_EMAIL = 'shreyash1833@gmail.com'
+EMAIL_HOST_USER = 'your_host_email'
+EMAIL_HOST_PASSWORD = 'your_host_password'
 
 
